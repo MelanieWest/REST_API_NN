cb097276dbbf10e64c3d83ce780323ea5339cff5
Melanie West <dhmofan@gmail.com>
2018-02-26 21:44:19 -0500

    still trying to find what's breaking

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,9 @@ const router = express.Router();
 //(get, post, etc) and instead of saying 'app.get' we'll say
 //'router.get', as shown:
 
-//router.get('/api/ninjas')
-//I want to get the route /api/ninjas, but since I'm in the api
-//file, I don't need the api at the front
-
 //get a list of ninjas from the database
 router.get('/ninjas', function(req,res){
+    //to test the route, we are just sending a simple response object
   res.send({type:'GET'});
 });
 
@@ -20,28 +17,14 @@ router.post('/ninjas', function(req,res){
 });
 
 //update a ninja in the db
-router.put('/ninjas/id', function(req,res){
+router.put('/ninjas/:id', function(req,res){
     res.send({type:'PUT'});
 });
 
 //delete a specific ninja from the db
-router.delete('/ninjas/id', function(req,res){
+router.delete('/ninjas/:id', function(req,res){
     res.send({type:'DELETE'});
 });
-
-//these were our practice routes to mess around.
-// // '/' is the request sent; 'req' and 'res' are data sent and received
-// app.get('/',function(req,res){
-//     console.log('GET request');
-//     res.end(); //this is a quick easy way to end the response so the blue circle stops spinning
-// });
-
-
-// // '/api' is the request sent; 'req' and 'res' are data sent and received
-// app.get('/api',function(req,res){
-//     console.log('GET request');
-//     res.send({name:"Yoshi"}); //this is a quick easy way to end the response so the blue circle stops spinning
-// });
 
 
 module.exports = router;
